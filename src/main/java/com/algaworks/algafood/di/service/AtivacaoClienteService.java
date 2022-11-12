5f21527a865ce4cdaacc20c1f14d69694874bf21
Valoir Leite Moura <valoir.moura@certisign.com.br>
2022-11-12 12:55:08 -0300

    2.15. Dependência opcional com @Autowired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -8,36 +8,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
+    @Autowired(required = false)
     private Notificador notificador;
-
-//    @Autowired
-//    public AtivacaoClienteService(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
-
-//    public AtivacaoClienteService(String qualquerCoisa) {
-//
-//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        this.notificador.notificar(cliente, "Seu Cadastro no Sistema está Ativo!");
+
+
+        if (notificador != null) {
+            this.notificador.notificar(cliente, "Seu Cadastro no Sistema está Ativo!");
+        } else {
+            System.out.println("Não Existe notificador, mas cliente foi ativado com sucesso!");
+        }
     }
 
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
 }
 
 
 /**
- * Ponto de Injeção 1: Construtor (Quando Houver apenas um construtor na Classe, não é necessário a anotação @Autowired, pois o Sprint injeta Automagicamente, entretando se houver
- * mais que um Construtor, faz-se necessário informar através da Anotação @Autowired, qual é o construtor que o Spring deverá injetar.
- *
- * Ponto de Injeção 2: Atráves do Set com a Anotação @Autowired
- *
- * Ponto de Injeção 3: é Diretamente no Atributo, com a Anotação @Autowired
- *
+ * Dependencia opcional do @Autowired
  */
