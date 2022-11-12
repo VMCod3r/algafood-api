131fd449473a428d11c1241df6f016ffb7b2f917
Valoir Leite Moura <valoir.moura@certisign.com.br>
2022-11-12 15:52:28 -0300

    2.25. Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,28 +1,26 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 
 
-    public NotificadorEmail() {
-        System.out.println("Notificador Email PROD!");
-    }
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta}")
+    private Integer porta;
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
+
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
-
-/**
- * @Qualifier: idenficação do Bean, quando for utilizar este bean, utiliza-se a mesma anotação para identificar qual BEAN será instanceado.
- *
- * Criando Anotações.
- */
