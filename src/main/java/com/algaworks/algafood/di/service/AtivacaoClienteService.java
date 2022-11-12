4fd45a93beb67722a108e78522847d90c993f3d1
Valoir Leite Moura <valoir.moura@certisign.com.br>
2022-11-12 14:01:44 -0300

    2.22. Publicando e consumindo eventos customizados

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,41 +1,20 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.util.List;
-
-//@Component
+@Component
 public class AtivacaoClienteService {
 
     @Autowired
-    @TipoDoNotificador(NivelUrgencia.NORMAL)
-    private Notificador notificador;
-
-
-
-//    @PostConstruct
-    public void  init() {
-        System.out.println("INIT!");
-    }
-
-
-//    @PreDestroy
-    public void  destroy() {
-        System.out.println("DESTROY!");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-      notificador.notificar(cliente, "Seu Cadastro no Sistema está Ativo!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 
     }
 
