d923d27a5d9ee523c0e210741d4e171d7a6b6afa
Valoir Leite Moura <valoir.moura@certisign.com.br>
2022-11-12 13:03:43 -0300

    2.16. Ambiguidade de beans e injeção de lista de beans

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -5,26 +5,27 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private Notificador notificador;
+    @Autowired
+    private List<Notificador> notificadores; //Para Notificar com todos os notificadores disponíveis.
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
+        //Neste Caso Notificados os Clientes com todos os Notificadores Disponiveis....
+        notificadores.forEach(notificador -> notificador.notificar(cliente, "Seu Cadastro no Sistema está Ativo!"));
 
-        if (notificador != null) {
-            this.notificador.notificar(cliente, "Seu Cadastro no Sistema está Ativo!");
-        } else {
-            System.out.println("Não Existe notificador, mas cliente foi ativado com sucesso!");
-        }
     }
 
 }
 
 
 /**
- * Dependencia opcional do @Autowired
+ * Quando há mais de um Bean, o Spring não sabe identificar qual deve instanciar: AMBIGUIDADE é o Termo.
+ *
+ * Faz-se Necessário realizar um processo de DESAMBIGUAÇÃO.
  */
